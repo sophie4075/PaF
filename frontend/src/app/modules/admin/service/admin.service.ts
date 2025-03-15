40a7279ea55423a4ac47594751de39b0db888bc1
Sophie <114300675+sophie4075@users.noreply.github.com>
2025-03-15 16:52:47 +0100

    Add dashboard api methods

--- frontend/src/app/modules/admin/service/admin.service.ts
+++ frontend/src/app/modules/admin/service/admin.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {StorageService} from "../../../shared/services/storage/storage.service";
+
+export interface AdminRentalInfoDto {
+  rentalPositionId: number;
+  rentalStart: string;
+  rentalEnd: string;
+  positionPrice: number;
+  userEmail: string;
+  userId: number;
+  articleDesignation: string;
+  articleInstanceInventoryNumber: string;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/rental';
+  constructor(private http: HttpClient) { }
+
+  getCurrentRentals(): Observable<AdminRentalInfoDto[]> {
+    return this.http.get<AdminRentalInfoDto[]>(`${this.apiUrl}/admin/current`);
+  }
+
+  getDueRentals(): Observable<AdminRentalInfoDto[]> {
+    return this.http.get<AdminRentalInfoDto[]>(`${this.apiUrl}/admin/due`);
+  }
+
+  getUpcomingUnderRepairRentals(): Observable<AdminRentalInfoDto[]> {
+    return this.http.get<AdminRentalInfoDto[]>(`${this.apiUrl}/admin/upcoming-under-repair`);
+  }
+
+  createAuthHeader(): HttpHeaders {
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set(
+        'Authorization',
+        'Bearer ' + StorageService.getToken()
+    )
+  }
 }
